a1f21c20945bfdec988f99a0bb5ed9c179dd7cbb
Julian Blagis <julianbgs96@gmail.com>
2023-12-13 22:52:50 +0300

    [~] 3 slide finish

--- src/main/Components/AchieveSlider/AchieveSlider.tsx
+++ src/main/Components/AchieveSlider/AchieveSlider.tsx
@@ -3,9 +3,9 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import {Navigation, Pagination, Scrollbar, A11y} from 'swiper/modules';
 
 import slideFirst from '../../../assets/images/slider/slide1.png';
-import slideSecond from '../../../assets/images/slider/slide1.png';
-import slideThird from '../../../assets/images/slider/slide1.png';
-import slideFourth from '../../../assets/images/slider/slide1.png';
+import slideSecond from '../../../assets/images/slider/slide2.png';
+import slideThird from '../../../assets/images/slider/slide3.png';
+import slideFourth from '../../../assets/images/slider/slide4.png';
 
 import './AchieveSlider.scss'
 import 'swiper/css';
@@ -52,9 +52,58 @@ export function AchievmentSlider(props: any) {
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="slide__body">
+                            <h3 className="slide__title">Доставка!</h3>
+                            <p className="slide__sub-title">«Фабрика» рада, что вы выбираете нашу доставку!</p>
+                            <p className="slide__description">Хотим сразу рассказать о тарифах по доставке, так как
+                                блюда мы отдаем по честной цене, нам придется включить стоимость за доставку:
+                            </p>
+                            <div className="slide__delivery">
+                                <p className="slide__delivery-item">Анапа – 150 рублей </p>
+                                <p className="slide__delivery-item slide__delivery-item--red">Витязево – 350 рублей</p>
+                                <p className="slide__delivery-item slide__delivery-item--blue">Анапская – 250 рублей</p>
+                                <p className="slide__delivery-item slide__delivery-item--purple">Джемете – 350 рублей</p>
+                                <p className="slide__delivery-item slide__delivery-item--orange">Су-Псех – 250 рублей
+                                </p>
+                            </div>
+                        </div>
+                        <div className="slide__image">
+                            <img src={slideSecond} alt="first slider"/>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="slide__body">
+                            <h3 className="slide__title">О важном!</h3>
+                            <p className="slide__sub-title">Хотим предостеречь вас о главном!</p>
+                            <p className="slide__description">Заказывая блюда на доставку убедитесь, что аллергены, присутствующие в составе отсутствуют, либо пропишите в комментариях к заказу. Вам обязательно прозвонит менеджер и уточнит все ваши пожелания
+
+                            </p>
+                        </div>
+                        <div className="slide__image">
+                            <img src={slideThird} alt="first slider"/>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide">
+                        <div className="slide__body">
+                            <h3 className="slide__title">Общая информация!</h3>
+                            <p className="slide__sub-title">Мы хотим, чтобы вы звонили нам!</p>
+                            <p className="slide__description">Бронь столов осуществляется по номерам:</p>
+                            <div className="slide__border-elem">
+                                <p className="slide__border-description">Мы также заинтересованы в классных кадрах,
+                                    поэтому будем рады любому сотрудничеству </p>
+                            </div>
+                        </div>
+                        <div className="slide__image">
+                            <img src={slideFourth} alt="first slider"/>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </div>
     )
