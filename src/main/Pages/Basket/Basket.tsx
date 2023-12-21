b09683e8e4ccf6da20d55617f0e9f0d5e6c1f912
Julian Blagis <julianbgs96@gmail.com>
2023-12-21 14:27:25 +0300

    [~] finish adaptive

--- src/main/Pages/Basket/Basket.tsx
+++ src/main/Pages/Basket/Basket.tsx
@@ -12,15 +12,63 @@ import {apiDev} from "../../Core/environment/api";
 import {saveBasket, saveItems} from "../../store/actions";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import {Field, Formik} from "formik";
+import Modal from "react-modal";
+import ChooseAddress from "../../Components/ChooseAddressModal/ChooseRestaurant";
+import {useMediaQuery} from "react-responsive";
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        border: 'none',
+        transform: 'translate(-50%, -50%)',
+        width: '715px',
+        height: '490px',
+        background: 'transparent'
+    },
+};
+
+
+const customStylesMobile = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        border: 'none',
+        transform: 'translate(-50%, -50%)',
+        width: '279px',
+        height: '490px',
+        background: 'transparent'
+    },
+};
 
 
 export default function Basket() {
     const dispatch = useDispatch();
 
+    const isMobile = useMediaQuery({
+        query: "(max-width: 786px)"
+    });
+    const isDesktop = useMediaQuery({
+        query: "(min-width: 1200px)"
+    });
+
     const state: InitialStoreInterface = useSelector((state: any) => state);
+    const address = state ? state.address : null;
 
     useEffect(() => {
         getBasket();
+
+        if (!address) {
+            openModal()
+        }
     }, [])
 
     function getBasket() {
@@ -54,8 +102,45 @@ export default function Basket() {
         return sum;
     }
 
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [modalIsOpenD, setIsOpenD] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function openModalD() {
+        setIsOpenD(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    function closeModalD() {
+        setIsOpenD(false);
+    }
+
     return (
         <>
+            {isMobile && <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Choose Cafe"
+                style={customStylesMobile}
+            >
+                <ChooseAddress closeModal={closeModal}/>
+            </Modal>
+            }
+            {isDesktop && <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Choose Cafe"
+                style={customStyles}
+            >
+                <ChooseAddress closeModal={closeModal}/>
+            </Modal>
+            }
             <Header/>
             <div className="container-small">
                 <section className="cart-main">
@@ -98,40 +183,119 @@ export default function Basket() {
                     {state?.items && state.items.length === 0 && <div className="empty">
                         Корзина пуста
                     </div>}
-                    <form className="form">
-                        <div className="form__align">
-                            <div className="form__box">
-                                <div className="form__title">
-                                    Заполните данные
+                    <Formik
+                        initialValues={{ email: '', name: '', number: '', address: '', picked: ''}}
+                        // validate={values => {
+                        //     const errors = {
+                        //         email: '',
+                        //         name: '',
+                        //         number: '',
+                        //         address: '',
+                        //         picked: '',
+                        //     };
+                        //     if (!values.email) {
+                        //         errors.email = 'Required';
+                        //     } else if (
+                        //         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        //     ) {
+                        //         errors.email = 'Invalid email address';
+                        //     }
+                        //
+                        //     if (!values.name.trim()) {
+                        //         errors.email = 'Required';
+                        //         errors.name = 'Required';
+                        //         errors.address = 'Required';
+                        //         errors.number = 'Required';
+                        //         errors.picked = 'Required';
+                        //     }
+                        //
+                        //     return errors;
+                        // }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+                                axios.post(`${apiDev}api/order`, {
+                                    address: {
+                                        street: values.address,
+                                    },
+                                    phoneNumber: values.number,
+                                    paymentMethod: +values.picked,
+                                    restaurantId: state.id
+                                }, {withCredentials: true})
+                                setSubmitting(false);
+                            }, 400);
+                        }}
+                    >
+                        {({
+                              values,
+                              errors,
+                              touched,
+                              handleChange,
+                              handleBlur,
+                              handleSubmit,
+                              isSubmitting,
+                              /* and other goodies */
+                          }) => (
+                            <form className="form" onSubmit={handleSubmit}>
+                                <div className="form__align">
+                                    <div className="form__box">
+                                        <div className="form__title">
+                                            Заполните данные
+                                        </div>
+                                        <input placeholder="Имя" className="form__input" type="text"
+                                               name="name"
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}
+                                               value={values.name}
+                                        />
+                                        <input placeholder="+7 (" className="form__input" type="text"
+                                               name="number"
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}
+                                               value={values.number}/>
+                                        <input placeholder="Адрес" className="form__input" type="text"
+                                               name="address"
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}
+                                               value={values.address}/>
+                                        <input placeholder="Email"
+                                               className="form__input"
+                                               type="email"
+                                               name="email"
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}
+                                               value={values.email}/>
+                                    </div>
+                                    <div className="form__box">
+                                        <div className="form__title">
+                                            Способ оплаты
+                                        </div>
+                                        <div className="radio__box">
+                                            <label>
+                                                <Field type="radio" name="picked" value="0" />
+                                                Наличные</label>
+                                        </div>
+                                        <div className="radio__box">
+                                            <label>
+                                            <Field type="radio" name="picked" value="1" />
+                                            Перевод на карту</label>
+                                        </div>
+                                        <div className="radio__box">
+                                            <label>
+                                                <Field type="radio" name="picked" value="2" />
+                                                Банковской картой курьеру</label>
+                                        </div>
+                                    </div>
                                 </div>
-                                <input placeholder="Имя" className="form__input" type="text"/>
-                                <input placeholder="+7 (" className="form__input" type="text"/>
-                                <input placeholder="Адрес" className="form__input" type="text"/>
-                            </div>
-                            <div className="form__box">
-                                <div className="form__title">
-                                    Способ оплаты
+                                <div className="form__center">
+                                    <input
+                                        type="submit"
+                                        className="submit"
+                                        value="Оформить заказ"
+                                    />
                                 </div>
-                                <div className="radio__box">
-                                    <input className="form__radio" name="payment" type="radio"/>
-                                    <label>Наличные</label>
-                                </div>
-                                <div className="radio__box">
-                                    <input className="form__radio" name="payment" type="radio"/>
-                                    <label>Перевод на карту</label>
-                                </div>
-                                <div className="radio__box">
-                                    <input className="form__radio" name="payment" type="radio"/>
-                                    <label>Банковской картой курьеру</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="form__center">
-                            <div className="submit">
-                                Форомить заказ
-                            </div>
-                        </div>
-                    </form>
+                            </form>
+                        )}
+                    </Formik>
                 </section>
             </div>
             <Footer/>
