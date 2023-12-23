acebe9a1ae71627e81f5adb557184cdff35a5aff
Julian Blagis <julianbgs96@gmail.com>
2023-12-23 16:06:21 +0300

    [~] parse Int

--- src/main/Pages/Basket/Basket.tsx
+++ src/main/Pages/Basket/Basket.tsx
@@ -172,7 +172,7 @@ export default function Basket() {
                                         </div>
                                     </div>
                                     <div className="cart-main__price">
-                                        {item.unitPrice * (item?.quantity ? item?.quantity : 1)}₽
+                                        {parseFloat((item.unitPrice * (item?.quantity ? item?.quantity : 1)).toString())}₽
                                     </div>
                                 </div>
                             })}
